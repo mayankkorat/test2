be90518f0a4bb816d795f7645ac0b5de93239267
mayankkorat <mayankkorat@gmail.com>
2025-02-04 16:51:31 +0530

    patch release

--- src/main.c
+++ src/main.c
@@ -4,14 +4,27 @@
 int main()
 {
     int a = 5, b = 3;
+    int sum, diff, prod, quotient;
 
     // Use functions from mathop.c
-    int   sum = add(a, b);
-    int   diff = subtract(a, b);
-    int   prod = multiply(a, b);
-    float quotient = divide(a, b);
+    sum = add(a, b);
+    diff = subtract(a, b);
+    prod = multiply(a, b);
+    quotient = divide(a, b);
 
-    /* Comment added to conflict it */
+    printf("Sum: %d\n", sum);
+    printf("Difference: %d\n", diff);
+    printf("Product: %d\n", prod);
+    printf("Quotient: %.2f\n", quotient);
+
+    a = 13;
+    b = 5;
+
+    sum = add(a, b);
+    diff = subtract(a, b);
+    prod = multiply(a, b);
+    quotient = divide(a, b);
+
     printf("Sum: %d\n", sum);
     printf("Difference: %d\n", diff);
     printf("Product: %d\n", prod);
