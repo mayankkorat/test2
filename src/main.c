78b90bcc740e6addfb92c902f817d2c7f3b55e10
mayankkorat <mayankkorat@gmail.com>
2025-02-04 16:12:17 +0530

    improve debugs in fomrat type 1

--- src/main.c
+++ src/main.c
@@ -1,20 +1,21 @@
 #include <stdio.h>
-#include "math_operations.h"  // Include the header file for math functions
+#include "mathop.h"  // Include the header file for math functions
 
-int main() {
+int main()
+{
     int a = 5, b = 3;
 
-    // Use functions from math_operations.c
-    int sum = add(a, b);
-    int diff = subtract(a, b);
-    int prod = multiply(a, b);
+    // Use functions from mathop.c
+    int   sum = add(a, b);
+    int   diff = subtract(a, b);
+    int   prod = multiply(a, b);
     float quotient = divide(a, b);
 
-    printf("Sum: %d\n", sum);
-    printf("Difference: %d\n", diff);
-    printf("Product: %d\n", prod);
-    printf("Quotient: %.2f\n", quotient);
+    /* Comment added to conflict it */
+    printf("sum: %d\n", sum);
+    printf("difference: %d\n", diff);
+    printf("product: %d\n", prod);
+    printf("quotient: %.2f\n", quotient);
 
     return 0;
 }
-
